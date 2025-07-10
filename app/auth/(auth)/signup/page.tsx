d8d232fc3db554e90e4544b6313b4e948db1f853
Raswanth7 <raswanthoff7@gmail.com>
2025-07-10 15:43:36 +0530

    Initial commit

---
+++ app/auth/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignUpForm } from './components/SignUpForm'
+
+const page = () => {
+  return (
+    <div>
+        <SignUpForm/>
+    </div>
+  )
+}
+
+export default page
